5ffcb93535a68a0c5c11cff63b2955b3d079a21d
WebTree-Baumgartner <baumgartner.webtree@gmail.com>
2017-12-14 11:40:29 +0100

    Implemented RabinKarp Algorithm
    Done:
     -Rolling Hash
     -Search Algorithm

--- ue7_1455128_patterns/src/com/webtree/Main.java
+++ ue7_1455128_patterns/src/com/webtree/Main.java
@@ -1,12 +1,17 @@
 package com.webtree;
 
 import com.webtree.algorithms.KMP;
+import com.webtree.algorithms.RabinKarp;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //KMP.search("xxx", "abcdexxxunbxxxxke");
+        System.out.println("KMP: ");
         System.out.println(KMP.search("xxx", "abcdexxxunbxxxxke"));
+
+        System.out.println("RabinKarp: ");
+        System.out.println(RabinKarp.search("xxx", "abcdexxxunbxxxxke"));
     }
 }
--- ue7_1455128_patterns/src/com/webtree/algorithms/RabinKarp.java
+++ ue7_1455128_patterns/src/com/webtree/algorithms/RabinKarp.java
@@ -1,25 +1,50 @@
 package com.webtree.algorithms;
 
 public class RabinKarp {
+    private static int count;
+    private static int index;
 
 
     public static Result search(String pattern, String text) throws IllegalArgumentException {
-        if (pattern == null || text == null)
-            throw new IllegalArgumentException("Provide pattern and/or text you want to search");
+        if (pattern == null || text == null || (pattern.length() > text.length()))
+            throw new IllegalArgumentException("Error with input");
         char[] pArray = pattern.toCharArray();
         int pHash = getHashValue(pArray);
-        return null;
+        int pos = 0;
+
+        while (pos + pattern.length() < text.length()) {
+            String tPart = ""; //used for matching
+            for (int i = 0; i < pattern.length(); i++) {
+                tPart += text.charAt(pos + i);
+            }
+            int tHash = getHashValue(tPart.toCharArray());
+            if (pHash == tHash) {
+                for (int i = 0; i < pattern.length(); i++) {
+                    if (pattern.charAt(i) != text.charAt(pos + i)) {
+                        break;
+                    } else if (i == pattern.length() - 1) {
+                        count++;
+                        index = pos;
+                    }
+                }
+            }
+            pos++;
+        }
+
+        return new Result(index, count);
     }
 
-    static private int getHashValue(char[] c){
-
-        return 0;
+    static private int getHashValue(char[] c) {
+        int vHash = 0;
+        for (int i = 0; i < c.length; i++) {
+            vHash = (c[i] * pow(10, (c.length - i - 1)) + vHash) % 101;
+        }
+        return vHash;
     }
 
-    static private int pow(int base, int exp){
-        return 0;
+    static private int pow(int base, int exp) {
+        return (int) Math.pow(base, exp);
     }
-
 
 
 }
